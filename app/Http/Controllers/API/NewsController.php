d6fa79e378444ed531fe68fc5ff1a8128b61fc00
fullcode100 <snowleopard722@outlook.com>
2023-06-26 04:53:01 +0200

    fixed: added filtering feature

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -11,8 +11,18 @@ class NewsController extends Controller
     public function index(Request $req)
     {
         $query = $req->query();
-        $url = 'https://newsapi.org/v2/everything?q=' . $query['searchKey'] . '&from=2023-06-21&' .
-            'to=2023-06-21&sortBy=popularity&apiKey=92b20f051922401495b91e004d3f0683';
+
+        $searchKey = isset($query['searchKey']) ? "q=" . $query['searchKey'] . "&" : "q= &";
+        $page = isset($query['page']) ? "page=" . $query['page'] . "&" : "";
+        $pageSize = isset($query['pageSize']) ? "pageSize=" . $query['pageSize'] . "&" : "";
+        $domains = isset($query['domains']) ? "domains=" . $query['domains'] . "&" : "";
+        $from = isset($query['from']) ? "from=" . $query['from'] . "&" : "";
+        $to = isset($query['to']) ? "to=" . $query['to'] . "&" : "";
+
+        $url = 'https://newsapi.org/v2/everything?' .
+            $searchKey . $page . $pageSize . $domains . $from . $to .
+            'apiKey=92b20f051922401495b91e004d3f0683';
+
         $client = new Client();
         $response = $client->get($url);
         $body = $response->getBody();
